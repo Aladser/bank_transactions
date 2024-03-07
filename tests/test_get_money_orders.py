f44a591c554fa40bde1e31ffa32881d0e4f622a2
Aladser <aladser@gmail.com>
2024-03-07 18:17:49 +0900

    обновлены тесты

--- tests/test_get_money_orders.py
+++ tests/test_get_money_orders.py
@@ -3,7 +3,9 @@ import os
 from utils import *
 import unittest
 
-test_file_path = '/home/aladser/projects/account_transactions/data/test_operations.json'
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+data_dir = f"{root_dir}/data/"
+test_file_path = f"{data_dir}test_operations.json"
 test_1_output = [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
                   'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
                   'description': 'Перевод организации', 'from': 'Maestro 1596837868705199',
@@ -15,7 +17,8 @@ test_1_output = [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:5
                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572',
                   'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}},
                   'description': 'Перевод организации', 'from': 'Счет 75106830613657916952',
-                  'to': 'Счет 11776614605963066702'}]
+                  'to': 'Счет 11776614605963066702'}
+                 ]
 test_2_ouput = [
     ['26.08.2019 Перевод организации', 'Maestro 1596 83** **** 5199 -> Счет **9589', '31957.58 руб.'],
     ['03.07.2019 Перевод организации', 'MasterCard 7158 30** **** 6758 -> Счет **5560', '8221.37 USD'],
